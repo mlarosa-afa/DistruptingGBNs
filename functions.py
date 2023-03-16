b1e277377d5fc289130d79b4c78ea1b9defadeff
c23matthew.larosa@afacademy.af.edu <c23matthew.larosa@afacademy.af.edu>
2023-03-16 16:15:38 +0000

    Consolidated Data

---
+++ functions.py
@@ -0,0 +1,211 @@
+import math
+import random
+import numpy as np
+from scipy.stats import invwishart
+from docplex.mp.model import Model
+
+#holds the subsets of Lambda
+class Lambda:
+    def __init__(self, Lambda_dot, evidence_vars, unobserved_vars):
+        self.yy = np.delete(Lambda_dot, evidence_vars, 1)
+        self.yy = np.delete(self.yy, evidence_vars, 0)
+        self.yz = np.delete(Lambda_dot, unobserved_vars, 1)
+        self.yz = np.delete(self.yz, evidence_vars, 0)
+        self.zy = np.transpose(self.yz)
+        self.zz = np.delete(Lambda_dot, unobserved_vars, 1)
+        self.zz = np.delete(self.zz, unobserved_vars, 0)
+
+class nonstandardVars:
+    def __init__(self, Q, vT, c, K,h,g, K_prime,h_prime,u_prime, S_prime, L):
+        self.Q, self.vT, self.c, self.K, self.h, self.g, self.K_prime, self.h_prime, self.u_prime, self.S_prime, self.L = Q, vT, c, K,h,g, K_prime,h_prime,u_prime, S_prime, L
+
+#Generates a Positive Definate square matrix of dimention num_dim.
+#There may be room for optimization here
+def generate_pos_def_matrix(num_dim, seed = 0):
+    if not seed == 0:
+        random.seed(seed)
+
+    #runs until matrix is Pos_def
+    while True:
+        # https://math.stackexchange.com/questions/332456/how-to-make-a-matrix-positive-semidefinite
+        random_matrix = np.random.rand(num_dim, num_dim)
+        PSD_Random = np.dot(random_matrix, random_matrix.transpose())
+        # check to see if PD, if so, continnue
+        if is_pos_def(PSD_Random):
+            break
+    return PSD_Random
+
+#Determins if given matrix X is positive definite
+def is_pos_def(x):
+    return np.all(np.linalg.eigvals(x) > 0)
+
+#Calculate the optimal (max) weights (phi_1 & phi_2)
+def  solve_optimal_weights(Q, vT, c, K_prime, u_prime, NUM_EVIDENCE_VARS):
+    qm = Model('DistruptionGBN')
+    z_DV = qm.continuous_var_matrix(1, NUM_EVIDENCE_VARS, name="Z_DV", lb=-3, ub=3)  # DV for decision variable
+
+    # Solve max KL first
+    Dmat = Q
+    Dvec = np.transpose(vT)
+    obj_fn = (list(z_DV.values()) @ Dmat @ list(z_DV.values())) + (Dvec @ list(z_DV.values()))
+    qm.set_objective("max", obj_fn)
+    qm.parameters.optimalitytarget.set(2)
+    qm.solve()
+    bestKL = qm.objective_value
+
+    # Solve marginal mode second
+    Dmat = -1 * (K_prime)
+    Dvec = 2 * np.matmul(K_prime, u_prime)
+    obj_fn = (list(z_DV.values()) @ Dmat @ list(z_DV.values())) + (Dvec @ list(z_DV.values()))
+    qm.set_objective("max", obj_fn)
+    qm.parameters.optimalitytarget.set(2)
+    qm.solve()
+    bestMM = qm.objective_value
+
+    return bestKL, bestMM
+
+#Select which variables are evidence given MVG prior and Mean
+#Optional Parameters:
+# Number of evidence variables to select. Will default to 25% of variables
+# evidence variables. list of rows. Will overwrite num of evidence vars if available
+def generate_evidence(MVG_Sigma, MVG_Mu, evidence_vars=[], NUM_EVIDENCE_VARS = 0, seed=3):
+    # If the number of evidence variables is not provided, make it 25% of the variables
+    if NUM_EVIDENCE_VARS == 0:
+        if not len(evidence_vars) == 0:
+            NUM_EVIDENCE_VARS = len(evidence_vars)
+        else:
+            NUM_EVIDENCE_VARS = math.floor(MVG_Mu.size * .25)
+
+
+    random.seed(seed)
+    # Generate Random evidence if none provided
+    if len(evidence_vars) == 0:
+        evidence_vars = random.sample(range(MVG_Mu.shape[0]), NUM_EVIDENCE_VARS)
+        evidence_vars.sort()
+
+    unobserved_vars = list(set(list(range(MVG_Mu.size))) - set(evidence_vars))
+
+    # Generates a new observed valued centered on MVG_Mu with sd of MVG_Sigma
+    np.random.seed(seed)
+    observed_vals = list()
+    for j in evidence_vars:
+        observed_vals.append(np.random.normal(MVG_Mu[j], MVG_Sigma[j, j], 1)[0])
+
+    return evidence_vars, unobserved_vars, observed_vals
+
+
+def params_from_sample(MVG_Sigma, MVG_Mu, evidence_vars, unobserved_vars, observed_vals):
+
+    # Is this inverse required?
+    Lambda_dot = np.linalg.inv(MVG_Sigma)  #Precision matrix of joint evidence distribution
+    eta_dot = np.matmul(Lambda_dot, MVG_Mu)
+    Xi_dot = -0.5 * np.matmul(np.matmul(np.transpose(MVG_Mu), Lambda_dot), MVG_Mu) - math.log((2*math.pi)**(MVG_Mu.size/2) * (np.linalg.det(MVG_Sigma) ** 0.5))
+
+    # Lambda Subsets  - Form Block Matrices from Joint Covariance Matrix
+    L = Lambda(Lambda_dot, evidence_vars, unobserved_vars)
+
+    Sigma_zz = np.delete(MVG_Sigma, unobserved_vars, 1)
+    Sigma_zz = np.delete(Sigma_zz, unobserved_vars, 0)
+
+    #eta Subsets
+    eta_y = np.delete(eta_dot, evidence_vars)
+    eta_z = np.delete(eta_dot, unobserved_vars)
+
+    # Conditional Distribution
+    K = L.yy
+    h = eta_y - np.matmul(L.yz, observed_vals)
+    g = Xi_dot + np.matmul(np.transpose(eta_z), observed_vals) - (0.5 * np.matmul(np.matmul(np.transpose(observed_vals), L.zz), observed_vals))
+
+    u_dot = np.matmul(np.linalg.inv(K), h)
+
+    #KL Divergence
+    K_prime = L.zz - np.matmul(np.matmul(L.zy,np.linalg.inv(L.yy)),L.yz)
+    h_prime = eta_z - np.matmul(L.zy, np.matmul(np.linalg.inv(L.yy), eta_y))
+
+    S_prime = np.linalg.inv(K_prime)
+    u_prime = np.matmul(S_prime, h_prime)
+
+    Q = np.matmul(np.matmul(np.transpose(L.yz), np.linalg.inv(K)), L.yz)
+    vT = 2 * (np.matmul(np.transpose(u_dot), L.yz) - np.matmul(np.matmul(np.transpose(eta_y), np.linalg.inv(K)), L.yz))
+    c = np.matmul(np.matmul(np.transpose(u_dot), K), u_dot) - 2 * np.matmul(np.transpose(eta_y), u_dot) + np.matmul(np.transpose(eta_y), np.matmul(np.linalg.inv(K), eta_y))
+    vals = nonstandardVars(Q, vT, c, K,h,g, K_prime,h_prime,u_prime, S_prime, L)
+    return vals
+
+#given optimals and eigenvalues, calculates concavity
+def identify_convavity(rho, Phi_1_opt, Zeta, Phi_2_opt, NUM_EVIDENCE_VARS):
+    #identify best constraints
+    b_concave = np.inf
+    b_convex = -np.inf
+    for rho_index in range(1, len(rho)+1):
+        for zeta_index in range(1, len(Zeta)+1):
+            const = ((Zeta[zeta_index-1] / Phi_2_opt) / ((rho[rho_index-1] / Phi_1_opt) + (Zeta[zeta_index-1] / Phi_2_opt)))
+            if rho_index + zeta_index - 1 <= NUM_EVIDENCE_VARS:
+                if const < b_concave:
+                    b_concave = const
+            if rho_index + zeta_index - NUM_EVIDENCE_VARS <= NUM_EVIDENCE_VARS and 1 <= rho_index + zeta_index - NUM_EVIDENCE_VARS:
+                if const > b_convex:
+                    b_convex = const
+
+    print(rho, Zeta)
+
+    return b_concave, b_convex
+
+#Identifys concavity given a prior
+#If no prior is passed, a random PD matrix of dim n is created
+def whitebox_preprocessing(MVG_Sigma=np.array([]), n=20, NUM_EVIDENCE_VARS = 0, seed = 3):
+
+    #checks to see if prior is passed. If not, generate a PD matrix as prior
+    if MVG_Sigma.size == 0:
+        MVG_Sigma = generate_pos_def_matrix(n)
+
+    # calculate row mean from cov matrix
+    MVG_Mu = np.mean(MVG_Sigma, axis=0)
+
+    evidence_vars, unobserved_vars, observed_vals = generate_evidence(MVG_Sigma, MVG_Mu, NUM_EVIDENCE_VARS = NUM_EVIDENCE_VARS, seed = seed)
+    v = params_from_sample(MVG_Sigma, MVG_Mu, evidence_vars, unobserved_vars, observed_vals)
+
+    Phi_opt1, Phi_opt2 = solve_optimal_weights(v.Q, v.vT, v.c, v.K_prime, v.u_prime, len(evidence_vars))
+
+    rho = np.sort(np.linalg.eigvals(v.Q))
+    Zeta = np.sort(np.linalg.eigvals(v.L.zz))
+
+    b_concave, b_convex = identify_convavity(rho, Phi_opt1, Zeta, Phi_opt2, len(evidence_vars))
+    return b_concave, b_convex, Phi_opt1, Phi_opt2, v, evidence_vars, observed_vals
+
+#SDG Methodss
+def check_gd_bounds(constraint, curr_pos, step):
+    next_step = curr_pos + step
+    #if(sum(constraint(curr_pos + step)) == len(curr_pos)):
+    #    return curr_pos + step
+    if next_step[0] > 3:
+        next_step[0] = 3
+    if next_step[1] > 3:
+        next_step[1] = 3
+    if next_step[2] > 3:
+        next_step[2] = 3
+    if next_step[0] < -3:
+        next_step[0] = -3
+    if next_step[1] < -3:
+        next_step[1] = -3
+    if next_step[2] < -3:
+        next_step[2] = -3
+
+    return next_step
+
+def adaGrad(gradient, constraint, curr_pos, learn_rate=0.001, v = 0):
+    step = (learn_rate / math.sqrt(v + (.00001))) * gradient(curr_pos)
+    v_t_1 = v + pow(np.linalg.norm(gradient(curr_pos)), 2)
+    return check_gd_bounds(constraint, curr_pos, step), v_t_1
+
+def RMSProp(gradient, constraint, curr_pos, learn_rate= 0.001, v = 0, beta = .9):
+    step = ((learn_rate / math.sqrt(v + (.00001))) * gradient(curr_pos))
+    v_t_1 = (beta * v) + ((1-beta) * pow(np.linalg.norm(gradient(curr_pos)), 2))
+    return check_gd_bounds(constraint, curr_pos, step), v_t_1
+
+def adam(gradient, constraint, curr_pos, learn_rate= 0.001, t = 0, v = 0, m = 0, beta_1 = .9, beta_2 = .999):
+    m_t_1 = (beta_1 * m) + ((1 - beta_1) * gradient(curr_pos))
+    v_t_1 = (beta_1 * v) + ((1 - beta_2) * pow(np.linalg.norm(gradient(curr_pos)), 2))
+    m_t_1_hat = m_t_1 / (1 - pow(beta_1, t))
+    v_t_1_hat = v_t_1 / (1 - pow(beta_2, t))
+    step = (learn_rate / (np.sqrt(v_t_1_hat) + .00001)) * m_t_1_hat
+    return check_gd_bounds(constraint, curr_pos, step), v_t_1, m_t_1
--- main.py
+++ main.py
@@ -1,166 +1,86 @@
-import math, random
+import random
+from functions import *
 from scipy.stats import invwishart, multivariate_normal
-import cplex
 import numpy as np
-import numpy.random
 
 from docplex.mp.model import Model
 
-def vals_from_priors(MVG_Sigma, MVG_Mu):
-    Lambda_dot = np.linalg.inv(MVG_Sigma)  #Precision matrix of joint evidence distribution
-    eta_dot = np.matmul(Lambda_dot, MVG_Mu)
-    Xi_dot = -0.5 * np.matmul(np.matmul(np.transpose(MVG_Mu), Lambda_dot), MVG_Mu) - math.log((2*math.pi)**(MVG_Mu.size/2) * (np.linalg.det(MVG_Sigma) ** 0.5))
-
-    random.seed(100)
-    """
-    #True Random values
-    evidence_vars = random.sample(range(MVG_Mu.size), NUM_EVIDENCE_VARS).sort()
-
-    observed_val = list()
-    for j in range(evidence_vars):
-        observed_val.append(np.random.normal(MVG_Mu[j], MVG_Sigma[j, j], 1))
-    """
-
-    # Values from R
-    evidence_vars = [0, 1]
-    #observed_vals = [-0.3877692, 125.8079697]
-    observed_vals = [0, 0]
-
-    unobserved_vars = list(set(list(range(MVG_Mu.size))) - set(evidence_vars))
-    # Lambda Subsets  - Form Block Matrices from Joint Covariance Matrix
-    Lambda_yy = np.delete(Lambda_dot, evidence_vars, 1)
-    Lambda_yy = np.delete(Lambda_yy, evidence_vars, 0)
-    #Lambda_yz = Lambda_dot[!(vars % in% evidence_vars), (vars % in% evidence_vars)]
-    Lambda_yz = np.delete(Lambda_dot, unobserved_vars, 1)
-    Lambda_yz = np.delete(Lambda_yz, evidence_vars, 0)
-    Lambda_zy = np.transpose(Lambda_yz)
-    Lambda_zz = np.delete(Lambda_dot, unobserved_vars, 1)
-    Lambda_zz = np.delete(Lambda_zz, unobserved_vars, 0)
-
-    #eta Subsets
-    eta_y = np.delete(eta_dot, evidence_vars)
-    eta_z = np.delete(eta_dot, unobserved_vars)
-
-    # Conditional Distribution
-    K = Lambda_yy
-    h = eta_y - np.matmul(Lambda_yz, observed_vals)
-    g = Xi_dot + np.matmul(np.transpose(eta_z),observed_vals) - (0.5 * np.matmul(np.matmul(np.transpose(observed_vals), Lambda_zz), observed_vals))
-
-    u_dot = np.matmul(np.linalg.inv(K), h)
-
-    #KL Divergence
-    K_prime = Lambda_zz - np.matmul(np.matmul(Lambda_zy,np.linalg.inv(Lambda_yy)),Lambda_yz)
-    h_prime = eta_z - np.matmul(Lambda_zy, np.matmul(np.linalg.inv(Lambda_yy), eta_y))
-
-    S_prime = np.linalg.inv(K_prime)
-    u_prime = np.matmul(S_prime, h_prime)
-
-    Q = np.matmul(np.matmul(np.transpose(Lambda_yz), np.linalg.inv(K)), Lambda_yz)
-    vT = 2 * (np.matmul(np.transpose(u_dot), Lambda_yz) - np.matmul(np.matmul(np.transpose(eta_y), np.linalg.inv(K)), Lambda_yz))
-    c = np.matmul(np.matmul(np.transpose(u_dot), K), u_dot) - 2 * np.matmul(np.transpose(eta_y), u_dot) + np.matmul(np.transpose(eta_y), np.matmul(np.linalg.inv(K), eta_y))
-    return Q, vT, c, K_prime, u_prime
-
-#Pulling Prior from bnlean's gaussian.test (R Package)
+# Pulling Prior from bnlean's gaussian.test (R Package)
 MVG_Sigma = np.array([
-              [2.222218,  2.405899,   13.16801,   2.767075,   6.961947,   -5.349946,  -16.24868],
-              [2.405899,  9.518770,   27.97397,   13.044076,  17.126274,  5.038906,   -21.37693],
-              [13.168007, 27.973973,  106.47261,  36.051377,  58.480076,  -16.357502, -108.62405],
-              [2.767075,  13.044076,  36.05138,   18.136263,  23.240560,  10.837644,  -24.44388],
-              [6.961947,  17.126274,  58.48008,   23.240560,  49.670134,  21.735843,  -41.97427],
-              [-5.349946, 5.038906,   -16.35750,  10.837644,  21.735843,  85.079790,  71.66548],
-              [-16.248684, -21.376927, -108.62405, -24.443880, -41.974271, 71.665482,  149.08423]])
-
-#MVG_Sig1ma =    np.array([
-#             [2,    2,   13,    2,   6,   -5,  -16],
-#             [2,     9,   27,   13,  17,  5,   -21],
-#             [13,   27,  106,  36,  58,  -16, -108],
-#             [2,    13,  36,   18,  23,  10,  -24],
-#             [6,    17,  58,   23,  49,  21,  -41],
-#             [-5,   5,   -16,  10,  21,  85,  71],
-#             [-16, -21, -108, -24, -41, 71,  149]])
-
-
-#MVG_Mu = np.array([-0.6486452095, -0.0004317675, -0.4559603790,  0.4691271083, -0.6564162950,  2.5464207005,  2.2379555670])
-#MVG_Mu = np.array( [5,5,5,5,5,5,5])
-MVG_Mu = np.array( [0,5,5,5,5,5,5])
+    [2.222218, 2.405899, 13.16801, 2.767075, 6.961947],
+    [2.405899, 9.518770, 27.97397, 13.044076, 17.126274],
+    [13.168007, 27.973973, 106.47261, 36.051377, 58.480076],
+    [2.767075, 13.044076, 36.05138, 18.136263, 23.240560],
+    [6.961947, 17.126274, 58.48008, 23.240560, 49.670134]])
 
 if __name__ == '__main__':
-    NUM_EVIDENCE_VARS = 2
 
-
-    print("Mode 1: Whitebox\nMode 2: Gray Box - Sample Average Approximation\nMode 3: Gray Box - Stochastic Gradient Descent\n")
+    print(
+        "Mode 1: Whitebox\nMode 2: Gray Box - Sample Average Approximation\nMode 3: Gray Box - Stochastic Gradient Descent\n")
     mode = int(input("Enter which version you would like to run: "))
     if mode == 1:
 
-        U_1 = float(input("Enter Weight 1: "))
-        U_2 = float(input("Enter Weight 2: "))
+        U_1 = float(input("Enter U_1: "))
+        U_2 = 1 - U_1
+        print("Caluclated weight 2 as ", U_2)
+        num_evidence = int(input("Number of Evidence Variables: "))
 
-        Q, vT, c, K_prime, u_prime = vals_from_priors(MVG_Sigma, MVG_Mu)
+        b_concave, b_convex, Phi_opt1, Phi_opt2, v, evidence_vars, observed_vals = whitebox_preprocessing(MVG_Sigma,
+                                                                                                          NUM_EVIDENCE_VARS=num_evidence, seed = 12)
+        print("The interesting range of weight 1 ranges from ", b_concave, " to ", b_convex)
 
         qm = Model('DistruptionGBN')
-        idx = {1: {1:[1] * NUM_EVIDENCE_VARS}}
-        z_DV = qm.continuous_var_matrix(1,NUM_EVIDENCE_VARS, name="Z_DV", lb=-3, ub=3) #DV for decision variable
+        idx = {1: {1: [1] * num_evidence}}
+        z_DV = qm.continuous_var_matrix(1, num_evidence, name="Z_DV", lb=-3, ub=3)  # DV for decision variable
 
-        Dmat = np.zeros((NUM_EVIDENCE_VARS, NUM_EVIDENCE_VARS))
-        Dvec = np.zeros(NUM_EVIDENCE_VARS)
+        with open("Pareto_Front.tsv", "w+") as f:
+            f.write("u_1\tu_2\tz_1\tz_2\tz_3\tObj\tϕ_1\tϕ_2\tEigenvaluesofDMat\n")
+            for i in np.arange(0.0, 1.0, 0.01):
+                U_1 = i
+                U_2 = 1-i
+                # Solve normalized problem
+                W_1 = U_1 / Phi_opt1
+                W_2 = U_2 / Phi_opt2
 
-        #Solve max KL first
-        Dmat = Q
-        Dvec = np.transpose(vT)
-        obj_fn = (list(z_DV.values()) @ Dmat @ list(z_DV.values())) + (Dvec @ list(z_DV.values()))
-        qm.set_objective("max", obj_fn)
-        qm.parameters.optimalitytarget.set(2)
-        qm.solve()
-        #qm.print_solution()
-        bestKL = qm.objective_value
+                Dmat = (W_1 * v.Q) - (W_2 * v.K_prime)
+                Dvec = W_1 * np.transpose(v.vT) + 2 * W_2 * np.matmul(v.K_prime, v.u_prime)
+                obj_fn = (list(z_DV.values()) @ Dmat @ list(z_DV.values())) + (Dvec @ list(z_DV.values()))
+                qm.set_objective("max", obj_fn)
 
-        #Solve marginal mode second
-        Dmat =  -1* ( K_prime)
-        Dvec = 2 * np.matmul(K_prime, u_prime)
-        obj_fn = (list(z_DV.values()) @ Dmat @ list(z_DV.values())) + (Dvec @ list(z_DV.values()) )
-        qm.set_objective("max", obj_fn)
-        qm.parameters.optimalitytarget.set(2)
-        qm.solve()
-        #qm.print_solution()
-        bestMM = qm.objective_value
+                eig = np.linalg.eigvals(Dmat)
 
-        #Solve normalized problem
-        W_1 = U_1 / bestKL
-        W_2 = U_2/ bestMM
-        Dmat = (W_1 * Q) - (W_2 * K_prime)
-        Dvec = W_1 * np.transpose(vT) + 2 * W_2 * np.matmul(K_prime, u_prime)
-        obj_fn = (list(z_DV.values()) @ Dmat @ list(z_DV.values())) + (Dvec @ list(z_DV.values()) )
-        qm.set_objective("max", obj_fn)
-        qm.parameters.optimalitytarget.set(3)
-        #qm.parameters.optimalitytarget.set(2)
-        qm.solve()
-        qm.print_solution()
+                qm.parameters.optimalitytarget.set(3)
+                #qm.parameters.optimalitytarget.set(2)
 
+                solutionset = qm.solve()
+                #z = np.array([qm.solution["Z_DV_0_0"], qm.solution["Z_DV_0_1"], qm.solution["Z_DV_0_2"]])
+                #phi_1 = z @ Dmat @ z
+                #phi_2 = Dvec @ z
+
+                #f.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n" % (U_1, U_2, qm.solution["Z_DV_0_0"], qm.solution["Z_DV_0_1"], qm.solution["Z_DV_0_2"], qm.objective_value, phi_1, phi_2, eig[0],eig[1],eig[2]))
 
     elif mode == 2:
-        numDim = 7# int(input("Enter number of Dimentions: "))
-        numSamples = 1000#int(input("Enter number of Samples: "))
-        numDf = 100 #int(input("Enter degrees of freedom: "))
+        numDim = 7  # int(input("Enter number of Dimentions: "))
+        numSamples = 1000  # int(input("Enter number of Samples: "))
+        numDf = 100  # int(input("Enter degrees of freedom: "))
 
-        W_1 = float(input("Enter Weight 1: "))
-        W_2 = float(input("Enter Weight 2: "))
+        W_1 = float(input("Enter U 1: "))
+        W_2 = float(input("Enter U 2: "))
+        NUM_EVIDENCE_VARS = int(input("Enter Number of Observered Variables: "))
 
-        PSD = []
-        for i in range(numDim):
-            temp = []
-            for j in range(numDim):
-                temp.append((random.random() + 0.1) * 100)
-            PSD.append(temp)
+        prior = generate_pos_def_matrix(numDim)
 
-        invWishart = invwishart.rvs(df=numDf, scale=np.identity(len(PSD)), size=numSamples)
+        invWishart = invwishart.rvs(df=numDf, scale=np.identity(len(prior)), size=numSamples)
 
-        #Q, vT, c, K_prime, u_prime
+
+        # Q, vT, c, K_prime, u_prime
         parameters = []
 
         for sample_Sigma in invWishart:
-            sample_Mu = multivariate_normal.rvs(cov = sample_Sigma)
-            Q, vT, c, K_prime, u_prime = vals_from_priors(sample_Sigma, sample_Mu)
-            parameters.append((Q, vT, c, K_prime, u_prime))
+            sample_Mu = multivariate_normal.rvs(cov=sample_Sigma)
+            evidence_vars, unobserved_vars, observed_vals = generate_evidence(sample_Sigma, sample_Mu, NUM_EVIDENCE_VARS=NUM_EVIDENCE_VARS, seed=3)
+            vals = params_from_sample(sample_Sigma, sample_Mu, evidence_vars, unobserved_vars, observed_vals)
+            parameters.append((vals.Q, vals.vT, vals.c, vals.K_prime, vals.u_prime))
 
         Dmat = np.zeros((NUM_EVIDENCE_VARS, NUM_EVIDENCE_VARS))
         Dvec = np.zeros(NUM_EVIDENCE_VARS)
@@ -170,25 +90,62 @@ if __name__ == '__main__':
             Dvec = W_1 * np.transpose(set[1]) + 2 * W_2 * np.matmul(set[3], set[4])
 
         qm = Model('DistruptionGBN')
-        #idx = {1: {1: [1] * NUM_EVIDENCE_VARS}}
-        z_DV = qm.continuous_var_matrix(1, NUM_EVIDENCE_VARS, name="Z_DV", lb=-10000, ub=10000)  # DV for decision variable
-
-        # For Testing
-        #Dmat = np.identity(3)
-        # Dmat[1,2] = 1
+        # idx = {1: {1: [1] * NUM_EVIDENCE_VARS}}
+        z_DV = qm.continuous_var_matrix(1, NUM_EVIDENCE_VARS, name="Z_DV", lb=-10000,
+                                        ub=10000)  # DV for decision variable
 
         # Add objective function
-
-        obj_fn = (list(z_DV.values()) @ Dmat @ list(z_DV.values())) + (Dvec @ list(z_DV.values()) )
+        obj_fn = (list(z_DV.values()) @ Dmat @ list(z_DV.values())) + (Dvec @ list(z_DV.values()))
         qm.set_objective("max", obj_fn)
 
         qm.parameters.optimalitytarget.set(2)
         qm.solve()
         qm.print_solution()
         print(np.linalg.eig(Dmat))
+
     elif mode == 3:
         print("running SGD")
 
+        numDim = 5 # int(input("Enter number of Dimentions: "))
+        numSamples = 100  # int(input("Enter number of Samples: "))
+        numDf = 10  # int(input("Enter degrees of freedom: "))
 
+        W_1 = 0#float(input("Enter U 1: "))
+        W_2 = 1 - W_1
+        NUM_EVIDENCE_VARS = 3 #float(input("Enter Number of observered Variables: "))
+
+        psi = generate_pos_def_matrix(numDim, seed = 19)
+
+        evidence_vars, unobserved_vars, observed_vals = generate_evidence(psi,  multivariate_normal.rvs(cov=psi),
+                                                                          NUM_EVIDENCE_VARS=NUM_EVIDENCE_VARS, seed = 19)
+
+        print("Please select a method:\n\t1.AdaGrad\n\t2.RMSProp\n\t3.Adam")
+        method = int(input("method:"))
+        LEARN_RATE = float(input("Learning Rate: "))
+
+        position = np.array([0, 0, 0])
+        prev_position = np.array([10000, 10000, 10000])
+        v = 0
+        t = 1
+        m = 0
+
+        #number of iters
+
+        while abs(np.linalg.norm(position-prev_position)) > .0001:
+            sample_cov = invwishart.rvs(df=numDf, scale=psi)
+            sample_Mu = multivariate_normal.rvs(cov=sample_cov)
+
+            vals = params_from_sample(sample_cov, sample_Mu, evidence_vars, unobserved_vars, observed_vals)
+            Dmat = ((W_1 * vals.Q) - (W_2 * vals.K_prime))
+            Dvec = W_1 * np.transpose(vals.vT) + 2 * W_2 * np.matmul(vals.K_prime, vals.u_prime)
+            prev_position = position
+            if method == 1:
+                position, v = adaGrad(lambda z: (Dmat + Dmat.transpose()) @ z + (Dvec), lambda z: z <= 3, position, LEARN_RATE, v = v)
+            elif method == 2:
+                position, v = RMSProp(lambda z: (Dmat + Dmat.transpose()) @ z + (Dvec), lambda z: z <= 3, position, LEARN_RATE, v=v)
+            elif method == 3:
+                position, v, m = adam(lambda z: (Dmat + Dmat.transpose()) @ z + (Dvec), lambda z: z <= 3, position, LEARN_RATE, t = t, v = v, m = m)
+                t = t + 1
+            print(position)
 
     print("Thanks for using this program")
